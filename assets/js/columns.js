8d038dd3c39334ab90e4e578c2d33bc1caadb520
Simon Zou <simon.zou@gmail.com>
2014-06-09 20:41:09 -0700

    hash shows on click

--- assets/js/columns.js
+++ assets/js/columns.js
@@ -3,21 +3,25 @@ $(document).ready(function(){
     // Bind the event.
     $(window).hashchange( function(){
       // Alerts every time the hash changes!
-      hash = location.hash;
-      if (hash in names)
+      if (!hashChangeIgnore)
       {
-        //console.log(names);
-        $("html, body").scrollTop($("#opinion").offset().top);        
-        // console.log(hash + ' is in names');
-        $(".modal").modal('hide');
-        $( hash ).modal('show');  
-      }
-      else {
-        // console.log(hash + 'is not a name');
-      }
+        hash = location.hash;
+        if (hash in names)
+        {
+          //console.log(names);
+          $("html, body").scrollTop($("#opinion").offset().top);        
+          // console.log(hash + ' is in names');
+          $(".modal").modal('hide');
+          $( hash ).modal('show');  
+        }
+        else {
+          // console.log(hash + 'is not a name');
+        }
+      }    
     });
 
     // Trigger the event (useful on page load).
     $(window).hashchange();
+    hashChangeIgnore = false;
   });;
 })
